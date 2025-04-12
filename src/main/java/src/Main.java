3850f5d0b6578cc99c3d715afd251779673bd923
goncalo-pires <goncalopires10@protonmail.com>
2025-04-12 17:10:45 +0100

    Âfirst commitÂ

---
+++ src/main/java/src/Main.java
@@ -0,0 +1,39 @@
+package src;
+
+public class Main {
+
+    public static void main(String[] args) {
+        executeGoodThread("Worker thread");
+        executeBadThread("Misbehaving thread");
+    }
+
+    public static void executeGoodThread(String name) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread name: " + Thread.currentThread().getName());
+                System.out.println("Thread priority: " + Thread.currentThread().getPriority());
+            }
+        });
+
+        thread.setName(name);
+        thread.setPriority(Thread.MAX_PRIORITY);
+        System.out.println("Before execution of thread " + Thread.currentThread().getName());
+        thread.start();
+        System.out.println("After execution of thread " + Thread.currentThread().getName());
+    }
+
+    public static void executeBadThread(String name) {
+        Thread thread = new Thread(() -> {
+            throw new RuntimeException("Intentional Exception");
+        });
+
+        thread.setName(name);
+        thread.setUncaughtExceptionHandler((t, e) -> {
+            System.out.println("A critical error happened in thread " + Thread.currentThread().getName() + ". The error is " + e.getMessage());
+        });
+
+        thread.start();
+    }
+
+}
